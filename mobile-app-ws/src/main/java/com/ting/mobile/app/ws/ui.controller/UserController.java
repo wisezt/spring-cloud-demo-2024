a76c88c7f7a4165302859e1f3fdc9bd2d62b4e96
DESKTOP-6DID7G9\Ting <wisezt@hotmail.com>
2024-08-05 05:27:04 +0200

    Add PathVariable and RequestParam

--- mobile-app-ws/src/main/java/com/ting/mobile/app/ws/ui.controller/UserController.java
+++ mobile-app-ws/src/main/java/com/ting/mobile/app/ws/ui.controller/UserController.java
@@ -6,23 +6,39 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
 
+
+//    @GetMapping
+//    public String getUsers() {
+//        return "get users was called";
+//    }
+
     @GetMapping
-    public String getUser(){
-        return "get user was called";
+    public String getUsers(
+            @RequestParam(value = "page", defaultValue = "1", required = false) int page,
+            @RequestParam(value = "limit", defaultValue = "10", required = false) int limit,
+            @RequestParam(value = "sort", defaultValue = "false", required = false) boolean sort
+    ) {
+        return "getUsers was called. page = " + page + "\tlimit = " + limit + "\tsort = " + sort;
+    }
+
+
+    @GetMapping(path = "/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "get user was called. userId = " + userId;
     }
 
     @PostMapping
-    public String createUser(){
+    public String createUser() {
         return "create user was called";
     }
 
     @PutMapping
-    public String updateUser(){
+    public String updateUser() {
         return "update user was called";
     }
 
     @DeleteMapping
-    public String deteleUser(){
+    public String deteleUser() {
         return "delete user was called";
     }
 
