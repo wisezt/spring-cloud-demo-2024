d037e7df0fdc21e4dab9f8c4cd753c6a48676576
DESKTOP-6DID7G9\Ting <wisezt@hotmail.com>
2024-08-06 10:58:18 +0200

    Section4 & 5: add eureka-server-demo and eureka-client-demo

---
+++ eureka-client-demo/src/main/java/com/ting/eureka/client/demo/controller/usersController.java
@@ -0,0 +1,16 @@
+package com.ting.eureka.client.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class usersController {
+
+    @GetMapping
+    public String getUser(){
+        return "Working!";
+    }
+
+}
---
+++ eureka-client-demo/src/test/java/com/ting/eureka/client/demo/EurekaClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ting.eureka.client.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
