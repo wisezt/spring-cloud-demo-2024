9b610ec3ccd97835f3d3dba3128c063194ac28ca
DESKTOP-6DID7G9\Ting <wisezt@hotmail.com>
2024-08-14 09:10:25 +0200

    76. Add @PostMapping for creating user

--- PhotoAppApiUsers/src/main/java/com/ting/photoapp/api/user/controller/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/ting/photoapp/api/user/controller/UserController.java
@@ -1,25 +1,29 @@
-//package com.ting.photoapp.api.user.controller;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/users")
-//public class UserController {
-//
-//    @GetMapping
-//    public String getUsers(){
-//        return "USRS";
-//    }
-//
-//    @GetMapping
-//    public String getUser01(){
-//        return "USER01";
-//    }
-//
-//    @GetMapping("/status/check")
-//    public String getStatus(){
-//        return "Working...";
-//    }
-//}
+package com.ting.photoapp.api.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String getUsers(){
+        return "USRS";
+    }
+
+
+    @GetMapping("/status/check")
+    public String getStatus(){
+        return "Working...";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "User Created";
+    }
+
+
+}
