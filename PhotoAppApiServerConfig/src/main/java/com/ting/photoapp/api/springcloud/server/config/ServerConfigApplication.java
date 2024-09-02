f990eda0d4804f59ca951bbd9df266aa7ca6ed2f
DESKTOP-6DID7G9\Ting <wisezt@hotmail.com>
2024-09-02 05:44:05 +0200

    add spring-cloud-config-server

--- PhotoAppApiServerConfig/src/main/java/com/ting/photoapp/api/springcloud/server/config/ServerConfigApplication.java
+++ PhotoAppApiServerConfig/src/main/java/com/ting/photoapp/api/springcloud/server/config/ServerConfigApplication.java
@@ -2,6 +2,7 @@ package com.ting.photoapp.api.springcloud.server.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
